bc822e5b6f7bb83b57855c897c44b0593517aaf2
giovanni <giovannibertho@gmail.com>
2026-05-18 21:59:54 -0300

    criação dos métodos do diagrama de classes

--- GestaoFrota/Model/Veiculo.cs
+++ GestaoFrota/Model/Veiculo.cs
@@ -11,11 +11,26 @@ namespace GestaoFrota.Model
         private string modelo;
         private string cor;
         private int ano;
-
+        private Abastecimento abastecimento;
         public string Placa { get => placa; set => placa = value; }
         public string Fabricante { get => fabricante; set => fabricante = value; }
         public string Modelo { get => modelo; set => modelo = value; }
         public string Cor { get => cor; set => cor = value; }
         public int Ano { get => ano; set => ano = value; }
+
+
+        public double MediaConsumo()
+        {
+            double litro = Convert.ToDouble(abastecimento.Litros);
+            double km = Convert.ToDouble(abastecimento.Km);
+            double media = litro / km;
+            return media;
+        }
+
+        public List<Abastecimento> ListaAbastecimento() 
+        {
+
+            return null;
+        }
     }
 }
